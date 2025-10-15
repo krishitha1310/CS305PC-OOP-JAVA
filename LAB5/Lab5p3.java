ab41c15a8c9a0c25191e463cf6cc5ae39f53c35b
krishitha1310 <krishitha174@gmail.com>
2025-10-15 12:26:26 +0530

    Add files via upload

---
+++ LAB5/Lab5p3.java
@@ -0,0 +1,89 @@
+/*Sample Input
+
+1
+2
+3
+4
+5
+6
+
+Sample Output
+
+1+2=3
+1+2+3=6
+1+2+3+4+5=15
+1+2+3+4+5+6=21
+*/
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+class Add{
+    public void add(int...a){
+        String s="";
+        int sum=0;
+        for(int i:a){
+            sum+=i;
+            if(i==a[a.length-1]){
+                s+=i+"=";
+            }
+            else{
+                s+=i+"+";
+            }
+        }
+        s+=sum;
+        System.out.println(s);
+     
+       
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+       try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			int n1=Integer.parseInt(br.readLine());
+			int n2=Integer.parseInt(br.readLine());
+			int n3=Integer.parseInt(br.readLine());
+			int n4=Integer.parseInt(br.readLine());
+			int n5=Integer.parseInt(br.readLine());
+			int n6=Integer.parseInt(br.readLine());
+			Add ob=new Add();
+			ob.add(n1,n2);
+			ob.add(n1,n2,n3);
+			ob.add(n1,n2,n3,n4,n5);	
+			ob.add(n1,n2,n3,n4,n5,n6);
+			Method[] methods=Add.class.getDeclaredMethods();
+			Set<String> set=new HashSet<>();
+			boolean overload=false;
+			for(int i=0;i<methods.length;i++)
+			{
+				if(set.contains(methods[i].getName()))
+				{
+					overload=true;
+					break;
+				}
+				set.add(methods[i].getName());
+				
+			}
+			if(overload)
+			{
+				throw new Exception("Overloading not allowed");
+			}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+	
+	
+
+}
+
+
