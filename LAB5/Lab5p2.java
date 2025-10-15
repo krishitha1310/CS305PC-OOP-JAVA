378fbdfca5c3035285b038e3a40cebde28a6a958
krishitha1310 <krishitha174@gmail.com>
2025-10-15 12:34:07 +0530

    Update Lab5p2.java

--- LAB5/Lab5p2.java
+++ LAB5/Lab5p2.java
@@ -1,3 +1,17 @@
+/*
+Sample Input 0
+3 5
+2 4
+0 0
+-1 -2
+-1 3
+Sample Output 0
+243
+16
+java.lang.Exception: n and p should not be zero.
+java.lang.Exception: n or p should not be negative.
+java.lang.Exception: n or p should not be negative.
+*/
 import java.util.Scanner;
 class MyCalculator {
     long power(int n,int p) throws Exception{
