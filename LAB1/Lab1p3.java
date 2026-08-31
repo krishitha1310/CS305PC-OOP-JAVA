27c95f5248f39887451dabb95cac0f4f85df7f08
krishitha1310 <krishitha174@gmail.com>
2026-08-31 23:15:31 +0530

    Update main method signature spacing
    Refactor main method signature formatting.

--- LAB1/Lab1p3.java
+++ LAB1/Lab1p3.java
@@ -1,6 +1,6 @@
 import  java.util.Scanner;
 class Lab1p3{
-	public static void main(String...args){
+	public static void main( String...args ){
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the number1");
 		int a=sc.nextInt();
